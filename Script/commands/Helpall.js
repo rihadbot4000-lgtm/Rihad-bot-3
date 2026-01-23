7c418a36a6afaf7aae43be83ee74496cf26b987b
rihadbot4000-lgtm <rihadbot4000@gmail.com>
2026-01-23 09:17:13 +0600

    Update Helpall.js

--- Script/commands/Helpall.js
+++ Script/commands/Helpall.js
@@ -36,10 +36,10 @@ ${allCommands.map(cmd => `║ ➔ ${cmd}`).join("\n")}
 
  
  const backgrounds = [
- "https://i.imgur.com/wu0iDqS.jpeg",
- "https://i.imgur.com/zqsuJnX.jpeg",
- "https://i.imgur.com/Huz3nAE.png",
- "https://i.imgur.com/wu0iDqS.jpeg"
+ "https://i.imgur.com/0Ge5qhI.jpeg",
+ "https://i.imgur.com/0Ge5qhI.jpeg",
+ "https://i.imgur.com/0Ge5qhI.jpeg",
+ "https://i.imgur.com/0Ge5qhI.jpeg"
  ];
  const selectedBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
  const imgPath = __dirname + "/cache/helpallbg.jpg";
